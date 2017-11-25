74a2868cbe7804548bf66af861525772b31147c3
Promie Yutasane <promie.yutasane@gmail.com>
2017-11-25 11:28:57 +1100

    Initial draft structure in place

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,1 +1,26 @@
 
+/*
+
+Logic Plan
+
+
+- Hide some id's when the button is pressed
+
+Time Session
+- Session Time - with the subtract and add button when pressed the number increased
+- For testing purposes, set the number to anyone as a placeholder
+- Reset number back to the original number
+
+- Audio import
+
+
+Break Session
+- Minimum session time is 5 minutes
+
+
+
+
+
+
+
+*/
