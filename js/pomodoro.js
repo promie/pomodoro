3f1415c8950ff322683112a3c5b86a4c5b9b2528
Promie Yutasane <promie.yutasane@gmail.com>
2017-12-02 06:34:07 +1100

    Functional buttons

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,8 +1,38 @@
 $(function(){
 
-const count = parseInt($('#breakSlice').html()),
-count = parseInt($('#breakSlice').html()),
-    
+let breakLength = parseInt($('#breakTime').html()),
+    sessionLength = parseInt($('#sessionTime').html());
+
+//Break Length Buttons
+$('#breakMinus').on('click', function(){
+    if(breakLength > 0) {
+        breakLength -= 1;
+        $('#breakTime').html(breakLength);
+    }
+});
+
+$('#breakPlus').on('click', function(){
+        breakLength += 1;
+        $('#breakTime').html(breakLength);
+});
+
+//Session Length Buttons
+$('#sessionMinus').on('click', function(){
+    if(sessionLength > 0) {
+        sessionLength -= 1;
+        $('#sessionTime').html(sessionLength);
+    }
+});
+
+$('#sessionPlus').on('click', function(){
+    sessionLength += 1;
+    $('#sessionTime').html(sessionLength);
+});
+
+
+
+
+
 
 
 
