3111a5f669072468f60aa91863ae68910c67324b
Promie Yutasane <promie.yutasane@gmail.com>
2017-12-02 06:00:22 +1100

    Code Cleanup

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,64 +1,14 @@
-// Decrease/Increase Break functionality
-const breakLength = document.getElementById('breakSlice');
-let breakValue = parseInt(breakLength.textContent);
+$(function(){
 
-const decreaseBreak = () => {
-    if(breakValue > 1){
-        breakLength.innerHTML = breakValue -= 1;
-    }
-}
-
-const increaseBreak = () => {
-    breakLength.innerHTML = breakValue += 1;
-}
-
-// Decrease/Increase Session Length functionality
-const sessionLength = document.getElementById('sessionSlice');
-const mainSessionLength = document.getElementById('mainSessionSlice');
-let sessionValue = parseInt(sessionLength.textContent);
-let mainSessionValue = parseInt(mainSessionLength.textContent);
-
-const decreaseSession = () => {
-    if(sessionValue > 1) {
-        sessionLength.innerHTML = sessionValue -= 1;
-        mainSessionLength.innerHTML = mainSessionValue -= 1;
-    }
-}
-
-const increaseSession = () => {
-    sessionLength.innerHTML = sessionValue += 1;
-    mainSessionLength.innerHTML = mainSessionValue += 1;
-} 
-
-// Reset button
-const reset = () => {
-    sessionLength.innerHTML = 5;
-    breakLength.innerHTML = 5;
-    mainSessionLength.innerHTML = 5;
-}
-
-
-//Main Functionality - Countdown
-
-//Countdown for Session
-/*
-
-Click Event
-var counter = setInterval(timer, 1000);
-
-
-
-const timer = () =>{
-    count -= 1;
+const count = parseInt($('#breakSlice').html()),
+count = parseInt($('#breakSlice').html()),
+    
 
 
 
 
-}
-
-
-*/
 
 
 
 
+});
