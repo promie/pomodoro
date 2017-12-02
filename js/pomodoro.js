fcca9f94418690a6627602728f312d3c6d27977c
Promie Yutasane <promie.yutasane@gmail.com>
2017-12-02 12:40:58 +1100

    Working code

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,80 +1,92 @@
-$(function(){
+$(document).ready(function(){
 
-const buzzer = $('#buzzer')[0];    
-let breakLength = parseInt($('#breakTime').html()),
-    sessionLength = parseInt($('#sessionTime').html());
+    $('#reset').hide();
+    $('#pause').hide();
 
-$('#myCanvas').on('click', function(){
-    let sessionCounter = setInterval(timer, 1000);
-    sessionLength *= 60;
-    breakLength *= 60;
-
-    function timer() {
-        sessionLength -= 1;
-
-        if(sessionLength === 0){
-            buzzer.play();
-            clearInterval(sessionCounter);
-            let breakCounter = setInterval(breakTimer, 1000);
-        }
-            $('#mainTime').html(sessionLength);
-
-            if(sessionLength % 60 >= 10) {
-                $('#mainTime').html(Math.floor(sessionLength/60) + ':' + sessionLength % 60);
-            }else{
-                $('#mainTime').html(Math.floor(sessionLength/60) + ':' + '0' + sessionLength % 60);
+    const buzzer = $('#buzzer')[0];    
+    let breakLength = parseInt($('#breakTime').html()),
+        sessionLength = parseInt($('#sessionTime').html());
+        
+    $('#play').on('click', function(){
+        let sessionCounter = setInterval(timer, 1000);
+        sessionLength *= 60;
+        breakLength *= 60;
+        $(this).hide();
+        $('#pause').show();
+    
+        function timer() {
+            $('#status').html('Session');
+            sessionLength -= 1;
+    
+            if(sessionLength === 0){
+                buzzer.play();
+                clearInterval(sessionCounter);
+                let breakCounter = setInterval(breakTimer, 1000);
             }
-
-            function breakTimer(){
-                $('#status').html('Break');
-                breakLength -= 1;
-
-                if(breakLength === 0) {
-                    clearInterval(breakCounter);
-                    clearnInterval(sessionCounter);
-                    buzzer.play();
-                }
-
-                $('#mainTime').html(breakLength);
-                if(breakLength % 60 >= 10) {
-                    $('#mainTime').html(Math.floor(breakLength/60) + ':' + breakLength % 60);
+                $('#mainTime').html(sessionLength);
+    
+                if(sessionLength % 60 >= 10) {
+                    $('#mainTime').html(Math.floor(sessionLength/60) + ':' + sessionLength % 60);
                 }else{
-                    $('#mainTime').html(Math.floor(breakLength/60) + ':' + '0' + breakLength % 60);
+                    $('#mainTime').html(Math.floor(sessionLength/60) + ':' + '0' + sessionLength % 60);
                 }
-            }
-    }
-
-});
-
-//Break Length Buttons
-$('#breakMinus').on('click', function(){
-    if(breakLength > 0) {
-        breakLength -= 1;
-        $('#breakTime').html(breakLength);
-    }
-});
-
-$('#breakPlus').on('click', function(){
-        breakLength += 1;
-        $('#breakTime').html(breakLength);
-});
-
-//Session Length Buttons
-$('#sessionMinus').on('click', function(){
-    if(sessionLength > 0) {
-        sessionLength -= 1;
+    
+                function breakTimer(){
+                    $('#status').html('Break');
+                    breakLength -= 1;
+    
+                    if(breakLength === 0) {
+                        clearInterval(breakCounter);
+                        buzzer.play();
+                        $('#reset').show();
+                    }
+    
+                    $('#mainTime').html(breakLength);
+                    if(breakLength % 60 >= 10) {
+                        $('#mainTime').html(Math.floor(breakLength/60) + ':' + breakLength % 60);
+                    }else{
+                        $('#mainTime').html(Math.floor(breakLength/60) + ':' + '0' + breakLength % 60);
+                    }
+                }
+        }
+    
+    });
+    
+    $('#breakMinus').on('click', function(){
+        if(breakLength > 1) {
+            breakLength -= 1;
+            $('#breakTime').html(breakLength);
+            $('#status').html('Break');
+            $('#mainTime').html(breakLength + ':00');
+        }
+    });
+    
+    $('#breakPlus').on('click', function(){
+            breakLength += 1;
+            $('#breakTime').html(breakLength);
+            $('#status').html('Break');
+            $('#mainTime').html(breakLength + ':00');
+    });
+    
+    $('#sessionMinus').on('click', function(){
+        if(sessionLength > 1) {
+            sessionLength -= 1;
+            $('#sessionTime').html(sessionLength);
+            $('#status').html('Session');
+            $('#mainTime').html(sessionLength + ':00');
+        }
+    });
+    
+    $('#sessionPlus').on('click', function(){
+        sessionLength += 1;
         $('#sessionTime').html(sessionLength);
-    }
+        $('#status').html('Session');
+        $('#mainTime').html(sessionLength + ':00');
+    });
+
+    $('#reset').on('click', function(){
+        location.reload();
+    })
+        
 });
 
-$('#sessionPlus').on('click', function(){
-    sessionLength += 1;
-    $('#sessionTime').html(sessionLength);
-});
-
-
-$('#reset').on('click', function(){
-    location.reload();
-});
-
-});
