11f32297b09f8698d566ce630b0e1b5b19232bd4
Promie Yutasane <promie.yutasane@gmail.com>
2017-12-03 20:29:55 +1100

    Refactored ES6 Success!

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
-    
-    init();
 
-    
-        
+    init(); 
+
 });
 
 let pause = true,
     pauseElement = document.getElementById('pause'),
+    playElement = document.getElementById('play'),
     errorMessage = document.getElementById('errorMessage'),
     breakLength,
-    sessionLength;
+    sessionLength,
+    sessionCounter,
+    breakCounter;
 
 const buzzer = document.getElementById('buzzer'),
     tickTok = document.getElementById('tickTok');
@@ -86,123 +87,64 @@ const playTickTok = () =>{
 
 //Refresh Function
 const reset = () =>{
-    init();
+    if(pause) {
+        init();
+    }
 }
 
-
-/** WORKING CODE
-
-let pause = true;
-
-$('#reset').hide();
-
-
-const buzzer = $('#buzzer')[0],
-tickTok = $('#tickTok')[0];    
-let breakLength = parseInt($('#breakTime').html()),
-sessionLength = parseInt($('#sessionTime').html());
-
-$('#myCanvas').on('click', function(){
-
-
-
-    $('#reset').hide();
-    $('#errorMessage').hide();
-    let sessionCounter = setInterval(timer, 1000);
+//Play-Pause Button Function
+const playButton = () =>{
+    pause = false;
+    errorMessage.style.display = 'none';
+    playElement.style.display = 'none';
+    pauseElement.style.display = 'block';
+    sessionCounter = setInterval(timer, 1000);
     sessionLength *= 60;
     breakLength *= 60;
-        
-    function timer() {
-        $('#status').html('SESSION');
-        sessionLength -= 1;
-        tickTok.play();
-
-        if(sessionLength === 0){
-            buzzer.play();
-            clearInterval(sessionCounter);
-            let breakCounter = setInterval(breakTimer, 1000);
-        }
-            $('#mainTime').html(sessionLength);
-
-            if(sessionLength % 60 >= 10) {
-                $('#mainTime').html(Math.floor(sessionLength/60) + ':' + sessionLength % 60);
-            }else{
-                $('#mainTime').html(Math.floor(sessionLength/60) + ':' + '0' + sessionLength % 60);
-            }
-
-            function breakTimer(){
-                $('#status').html('BREAK');
-                breakLength -= 1;
-                tickTok.play();
-
-                if(breakLength === 0) {
-                    buzzer.play();
-                    $('#reset').show();
-
-                    for(let i=1; i<99999;i++){
-                        window.clearInterval(i);
-                    }
-                    pause = true;
-                }
-
-                $('#mainTime').html(breakLength);
-                if(breakLength % 60 >= 10) {
-                    $('#mainTime').html(Math.floor(breakLength/60) + ':' + breakLength % 60);
-                }else{
-                    $('#mainTime').html(Math.floor(breakLength/60) + ':' + '0' + breakLength % 60);
-                }
-            }
-    }        
-});
-
-$('#breakMinus').on('click', function(){
-if(breakLength > 1 && pause === true) {
-    breakLength -= 1;
-    $('#errorMessage').hide();
-    $('#breakTime').html(breakLength);
-    $('#status').html('BREAK');
-    $('#mainTime').html(breakLength + ':00');
-}else{
-    $('#errorMessage').show();
-    $('#errorMessage').html('BREAK LENGTH CANNOT BE LESS THAN 1 MINUTE');
 }
-});
 
-$('#breakPlus').on('click', function(){
-if(pause === true){
-    $('#errorMessage').hide();
-    breakLength += 1;
-    $('#breakTime').html(breakLength);
-    $('#status').html('BREAK');
-    $('#mainTime').html(breakLength + ':00');
+const pauseButton = () =>{
+    errorMessage.style.display = 'none';
+    pauseElement.style.display = 'none';
+    playElement.style.display = 'block';
 }
-});
 
-$('#sessionMinus').on('click', function(){
-if(sessionLength > 1 && pause === true) {
+const timer = () =>{
+    document.getElementById('status').innerHTML = 'SESSION';
     sessionLength -= 1;
-    $('#errorMessage').hide();
-    $('#sessionTime').html(sessionLength);
-    $('#status').html('SESSION');
-    $('#mainTime').html(sessionLength + ':00');
-}else{
-    $('#errorMessage').show();
-    $('#errorMessage').html('SESSION LENGTH CANNOT BE LESS THAN 1 MINUTE');
+    playTickTok();
+
+    if(sessionLength === 0) {
+        playBuzzer();
+        clearInterval(sessionCounter);
+        breakCounter = setInterval(breakTimer, 1000);
+    }
+
+    document.getElementById('mainTime').innerHTML = sessionLength;
+
+    if(sessionLength % 60 >= 10) {
+        document.getElementById('mainTime').innerHTML = `${Math.floor(sessionLength / 60)}:${sessionLength % 60}`;
+    }else{
+        document.getElementById('mainTime').innerHTML = `${Math.floor(sessionLength / 60)}:0${sessionLength % 60}`;
+    }
 }
-});
 
-$('#sessionPlus').on('click', function(){
-if(pause === true) {
-    sessionLength += 1;
-    $('#errorMessage').hide();
-    $('#sessionTime').html(sessionLength);
-    $('#status').html('SESSION');
-    $('#mainTime').html(sessionLength + ':00');    
+const breakTimer = () =>{
+    document.getElementById('status').innerHTML = 'BREAK';
+    breakLength -= 1;
+    playTickTok();
+
+    if(breakLength === 0){
+        playBuzzer();
+        clearInterval(breakCounter);
+        pause = true;
+    }
+
+    document.getElementById('mainTime').innerHTML = breakLength;
+
+    if(breakLength % 60 >= 10) {
+        document.getElementById('mainTime').innerHTML = `${Math.floor(breakLength / 60)}:${breakLength % 60}`;
+    }else{
+        document.getElementById('mainTime').innerHTML = `${Math.floor(breakLength / 60)}:0${breakLength % 60}`;
+    }
 }
-});
-
-$('#reset').on('click', function(){
-location.reload();
-})
-
-**/
